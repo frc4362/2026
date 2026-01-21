ee31aae1611f4c1d2a5beb4617422b25046aec7b
MathWizKid <adamjarrett2010@gmail.com>
2026-01-20 20:13:50 -0500

    Intake sim test
    Intake sim in a position to be tested

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import com.ctre.phoenix6.swerve.SwerveModule;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.swerve.CommandSwerveDrivetrain;
 import frc.robot.subsystems.swerve.TunerConstants;
 
@@ -27,11 +28,14 @@ public class RobotContainer {
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
+    private Intake m_intake;
+
     private final CommandXboxController joystick = new CommandXboxController(0);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
     public RobotContainer() {
+        m_intake = new Intake();
         configureBindings();
     }
 
@@ -55,5 +59,8 @@ public class RobotContainer {
         );
 
         drivetrain.registerTelemetry(logger::telemeterize);
+
+        joystick.a().onTrue(m_intake.setIntakeVoltage(6.0));
+        joystick.a().onFalse(m_intake.setIntakeVoltage(0.0));
     }
 }
