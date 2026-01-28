38acc26022a7c1410857d5b4faff13511f3ca3b7
Ethan Malzone <ejmalzone@gmail.com>
2026-01-28 16:26:22 -0500

    Update Hood.java for different frames of reference (incomplete)

--- src/main/java/com/gemsrobotics/subsystems/superstructure/Hood.java
+++ src/main/java/com/gemsrobotics/subsystems/superstructure/Hood.java
@@ -1,5 +1,6 @@
 package com.gemsrobotics.subsystems.superstructure;
 
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -10,16 +11,20 @@ import com.gemsrobotics.lib.StatusSignalManager;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
-import java.time.Period;
-
 public class Hood {
     private static final double GEARING = 150.0;
-    private static final Rotation2d STARTING_ANGLE = Rotation2d.fromDegrees(75.0);
-    private static final double SIM_PERIOD = 0.002;
+    // 15 degrees forward from the vertical
+    public static final Rotation2d STARTING_ANGLE = Rotation2d.fromDegrees(15.0);
+    private static final double SIM_PERIOD = 0.001;
 
     private final TalonFX m_motor;
     private final PositionTorqueCurrentFOC m_request;
@@ -28,6 +33,10 @@ public class Hood {
     private final TalonFXSimState m_simState;
     private final Notifier m_simNotifier;
     private final DCMotorSim m_motorModel;
+
+    private final StatusSignal<Angle> m_motorRotations;
+    private final StatusSignal<Current> m_motorAmps;
+    private final StructPublisher<Rotation2d> m_worldAnglePublisher;
 
     public Hood(final StatusSignalManager signalManager, final TalonFX motor) {
         m_motor = motor;
@@ -45,28 +54,48 @@ public class Hood {
         m_request = new PositionTorqueCurrentFOC(0.0);
         m_request.Slot = 0;
 
+        m_motorRotations = m_motor.getRotorPosition(false);
+        m_motorAmps = m_motor.getTorqueCurrent(false);
+
+        final NetworkTable nt = NetworkTableInstance.getDefault().getTable("hood");
+        m_worldAnglePublisher = nt.getStructTopic("rotations_world", Rotation2d.struct).publish();
+        signalManager.registerPublished(m_motorRotations, nt, "rotations_motor");
+        signalManager.registerPublished(m_motorAmps, nt, "amps");
+
         // sim
+        m_motorModel = new DCMotorSim(
+                LinearSystemId.createDCMotorSystem(DCMotor.getKrakenX44Foc(1), 0.001, GEARING),
+                DCMotor.getKrakenX44Foc(1));
+
         m_simState = m_motor.getSimState();
         m_simNotifier = new Notifier(this::simulationPeriodic);
         if (Robot.isSimulation()) {
             m_simNotifier.startPeriodic(SIM_PERIOD);
         }
-
-        m_motorModel = new DCMotorSim(
-                LinearSystemId.createDCMotorSystem(DCMotor.getKrakenX44Foc(1), 0.001, GEARING),
-                DCMotor.getKrakenX44Foc(1));
     }
 
-    public void setGoal(final Rotation2d goal) {
+    public void periodic() {
+        m_worldAnglePublisher.set(getWorldAngle());
+    }
+
+    public void setWorldAngle(final Rotation2d goal) {
         m_motor.setControl(m_request.withPosition(angle2Rotor(goal)));
     }
 
     private double angle2Rotor(final Rotation2d angle) {
-        return angle.minus(STARTING_ANGLE).getRotations();
+        return angle.getRotations();
     }
 
     private Rotation2d rotor2Angle(final double rotations) {
-        return Rotation2d.fromRotations(rotations).plus(STARTING_ANGLE);
+        return Rotation2d.fromRotations(rotations);
+    }
+
+    public Rotation2d getWorldAngle() {
+        return rotor2Angle(m_motorRotations.getValueAsDouble()).plus(STARTING_ANGLE);
+    }
+
+    private double clampSafeRotor(final double rotorPosition) {
+        return 0.0;
     }
 
     private void simulationPeriodic() {
