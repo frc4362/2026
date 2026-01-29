f38ae0472ff72dafb69348bfc86ea0b34cbc6c41
Ethan Malzone <ejmalzone@gmail.com>
2026-01-29 01:41:17 -0500

    Tune shot from NetworkTables

---
+++ src/main/java/com/gemsrobotics/shooting/TunedLaunchStrategy.java
@@ -0,0 +1,34 @@
+package com.gemsrobotics.shooting;
+
+import com.gemsrobotics.subsystems.superstructure.Hood;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.NetworkTable;
+
+// test launcher parameters tuned from NetworkTables
+public final class TunedLaunchStrategy extends LaunchStrategy {
+	private static final Rotation2d MIN_ANGLE = Hood.MIN_ANGLE;
+
+	private final DoubleSubscriber m_hoodAngleSubscriber, m_rpsSubscriber;
+
+	public TunedLaunchStrategy(final NetworkTable nt) {
+		m_hoodAngleSubscriber = nt.getDoubleTopic("hood_angle_degrees").subscribe(Hood.MIN_ANGLE.getDegrees());
+		m_rpsSubscriber = nt.getDoubleTopic("velocity_rps").subscribe(0.0);
+	}
+
+	@Override
+	protected LaunchParameters unsafeParametersFor(final double rangeMetersUnused) {
+		return new LaunchParameters(Rotation2d.fromDegrees(m_hoodAngleSubscriber.get()), m_rpsSubscriber.get());
+	}
+
+	@Override
+	public double getMinRangeMeters() {
+		return 0.0;
+	}
+
+	@Override
+	public double getMaxRangeMeters() {
+		// lets be fr
+		return 15.0;
+	}
+}
