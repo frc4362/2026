098f5044533be0bd9fe8d2495fd940f97812cdc5
Ethan Malzone <ejmalzone@gmail.com>
2026-02-04 19:53:25 -0500

    Start vision impl

---
+++ src/main/java/com/gemsrobotics/vision/Vision.java
@@ -0,0 +1,10 @@
+package com.gemsrobotics.vision;
+
+import com.gemsrobotics.RobotState;
+import edu.wpi.first.networktables.NetworkTable;
+
+public final class Vision {
+    public Vision(final RobotState robotState, final NetworkTable launcherLimelight, final NetworkTable climberLimelight) {
+
+    }
+}
