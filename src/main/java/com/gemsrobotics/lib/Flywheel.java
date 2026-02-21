df753a211d8125c2bd18330d8c4fc4676ec72d13
CraftyDWB <danielwb416@gmail.com>
2026-02-21 09:34:52 -0500

    Flywheel request implementation

--- src/main/java/com/gemsrobotics/lib/Flywheel.java
+++ src/main/java/com/gemsrobotics/lib/Flywheel.java
@@ -1,6 +1,8 @@
 package com.gemsrobotics.lib;
 
 import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.CoastOut;
+import com.ctre.phoenix6.controls.MotionMagicVelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -9,10 +11,15 @@ import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.Temperature;
 
+import static edu.wpi.first.units.Units.Meters;
+
 public class Flywheel {
-    private final Distance m_wheelRadius;
+    private final double m_wheelRadiusMeters;
     private final TalonFX m_motorLeader;
-    private final TalonFX[] m_motorFollowers;
+    //private final TalonFX[] m_motorFollowers;
+
+    private final MotionMagicVelocityTorqueCurrentFOC m_request;
+    private final CoastOut m_coastRequest;
 
     private final StatusSignal<AngularVelocity> m_leaderVelocitySignal;
     private final StatusSignal<Double> m_leaderVelocityReferenceSignal;
@@ -25,9 +32,13 @@ public class Flywheel {
                     final Distance wheelRadius,
                     final TalonFX motorLeader,
                     final TalonFX... motorFollowers) {
-        m_wheelRadius = wheelRadius;
+        m_wheelRadiusMeters = wheelRadius.in(Meters);
         m_motorLeader = motorLeader;
-        m_motorFollowers = motorFollowers;
+        //m_motorFollowers = motorFollowers;
+
+        m_request = new MotionMagicVelocityTorqueCurrentFOC(0);
+
+        m_coastRequest = new CoastOut();
 
         m_leaderVelocitySignal = m_motorLeader.getVelocity();
         m_leaderVelocityReferenceSignal = m_motorLeader.getClosedLoopReference();
@@ -54,5 +65,18 @@ public class Flywheel {
                 wheelRadius,
                 motorLeader,
                 motorFollowers);
+    }
+
+    public void setAngularVelocity(double rps) {
+        m_motorLeader.setControl(m_request.withVelocity(rps));
+    }
+
+    public void setLinearVelocity(double mps) {
+        final double circumference = 2.0 * Math.PI * m_wheelRadiusMeters;
+        setAngularVelocity(mps / circumference);
+    }
+
+    public void setOff() {
+        m_motorLeader.setControl(m_coastRequest);
     }
 }
