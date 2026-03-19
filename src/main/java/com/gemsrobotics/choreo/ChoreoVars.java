f1eda8c7e3c68dd99baeebc5bb55059a0ae6aee4
MathWizKid <adamjarrett2010@gmail.com>
2026-03-18 20:11:41 -0400

    Autos Changes
    Made some of the autos based on variables, made a version that looks at tags while it moves.

--- src/main/java/com/gemsrobotics/choreo/ChoreoVars.java
+++ src/main/java/com/gemsrobotics/choreo/ChoreoVars.java
@@ -1,7 +1,6 @@
+// spotless:off
 package com.gemsrobotics.choreo;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.*;
 
@@ -11,6 +10,52 @@ import edu.wpi.first.units.measure.*;
  * in the Choreo GUI.
  */
 public final class ChoreoVars {
+    public static final Distance fieldx = Units.Meters.of(16.54);
+    public static final Distance fieldy = Units.Meters.of(8.07);
+    public static final Distance midx = Units.Meters.of(8.27);
+    public static final Distance midy = Units.Meters.of(4.035);
+    public static final Angle pass1r1 = Units.Radians.of(0.8591);
+    public static final Angle pass1r2 = Units.Radians.of(2.1657);
+    public static final Angle pass1r3 = Units.Radians.of(1.5946);
+    public static final Angle pass1r4 = Units.Radians.of(1.5708);
+    public static final Angle pass1r5 = Units.Radians.of(-1.3597);
+    public static final Angle pass1r6 = Units.Radians.of(-1.3597);
+    public static final Distance pass1x1 = Units.Meters.of(2.5151);
+    public static final Distance pass1x2 = Units.Meters.of(1.6104);
+    public static final Distance pass1x3 = Units.Meters.of(0.3575);
+    public static final Distance pass1x4 = Units.Meters.of(0.4112);
+    public static final Distance pass1x5 = Units.Meters.of(2.2128);
+    public static final Distance pass1x6 = Units.Meters.of(4.7984);
+    public static final Distance pass1y1 = Units.Meters.of(1.5125);
+    public static final Distance pass1y2 = Units.Meters.of(3.1669);
+    public static final Distance pass1y3 = Units.Meters.of(2.97);
+    public static final Distance pass1y4 = Units.Meters.of(0.7685);
+    public static final Distance pass1y5 = Units.Meters.of(1.3099);
+    public static final Distance pass1y6 = Units.Meters.of(1.3017);
+    public static final Angle pass2r1 = Units.Radians.of(0.6394);
+    public static final Angle pass2r2 = Units.Radians.of(0.6394);
+    public static final Angle pass2r3 = Units.Radians.of(2.1017);
+    public static final Angle pass2r4 = Units.Radians.of(1.5708);
+    public static final Angle pass2r5 = Units.Radians.of(1.5886);
+    public static final Angle pass2r6 = Units.Radians.of(2.4164);
+    public static final Angle pass2r7 = Units.Radians.of(2.4164);
+    public static final Distance pass2x1 = Units.Meters.of(5.4408);
+    public static final Distance pass2x2 = Units.Meters.of(2.391);
+    public static final Distance pass2x3 = Units.Meters.of(1.5388);
+    public static final Distance pass2x4 = Units.Meters.of(0.3754);
+    public static final Distance pass2x5 = Units.Meters.of(0.4088);
+    public static final Distance pass2x6 = Units.Meters.of(2.4461);
+    public static final Distance pass2x7 = Units.Meters.of(5.5587);
+    public static final Distance pass2y1 = Units.Meters.of(1.5039);
+    public static final Distance pass2y2 = Units.Meters.of(1.5679);
+    public static final Distance pass2y3 = Units.Meters.of(3.1327);
+    public static final Distance pass2y4 = Units.Meters.of(2.9358);
+    public static final Distance pass2y5 = Units.Meters.of(0.7215);
+    public static final Distance pass2y6 = Units.Meters.of(1.737);
+    public static final Distance pass2y7 = Units.Meters.of(1.7903);
 
-    private ChoreoVars() {}
+    public static final class Poses {
+
+    }
 }
+// spotless:on
